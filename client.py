372722ccbbdd33b4a0f2905858f8b3bad6c83242
yuarmy0921 <christylin890921@gmail.com>
2022-03-25 22:57:08 +0800

    done

---
+++ client.py
@@ -0,0 +1,103 @@
+from bluepy.btle import Peripheral, UUID                                     
+from bluepy.btle import Scanner, DefaultDelegate
+
+class ScanDelegate(DefaultDelegate):            
+        def __init__(self):                                                  
+                DefaultDelegate.__init__(self)  
+                self.index = 0
+                self.l = ['x', 'y', 'z']
+        def handleDiscovery(self, dev, isNewDev, isNewData):                 
+                if isNewDev:                                                 
+                        print ("Discovered device", dev.addr)                
+                elif isNewData:                                              
+                        print ("Received new data from", dev.addr)     
+        def handleNotification(self, cHandle, data):
+            #super().handleNotification()
+            if cHandle == 13:
+                print("heart rate:", ord(data.decode()[-1]))
+            else:
+                print(data)
+                first = data[1:2]
+                #print(type(first))
+                second = data[2:]
+                new_byte_string = second + first
+                print(first, second, new_byte_string)
+                num = int.from_bytes(new_byte_string, 'big', signed=True)
+                
+                print(f"magnetic flux {self.l[self.index]}: {num}")
+                  
+                self.index += 1
+                if self.index == 3:
+                    self.index = 0
+                
+scanner = Scanner().withDelegate(ScanDelegate())                             
+devices = scanner.scan(10.0)
+#device.setDelegate(ScanDelegate)
+n=0                                                                          
+addr = []                                                                    
+for dev in devices:                                                          
+        print ("%d: Device %s (%s), RSSI=%d dB" % (n, dev.addr, dev.addrType, dev.rssi))
+        addr.append(dev.addr)                                                
+        n += 1                                                               
+        for (adtype, desc, value) in dev.getScanData():                      
+                print (" %s = %s" % (desc, value))                           
+number = input('Enter your device number: ')                                 
+print ('Device', number)                                                     
+num = int(number)                                                            
+print (addr[num])                                                            
+#                                                                            
+print ("Connecting...")
+while True:
+    try:
+        dev = Peripheral(addr[num], 'random')
+        break
+    except:
+        pass
+dev.setDelegate(ScanDelegate())
+#for ch in testService.getCharacteristics():
+#                                                                            
+print ("Services...")                                                        
+for svc in dev.services:                                                     
+    print (str(svc))
+
+#                                                                            
+#try:
+
+#testService = dev.getServiceByUUID(UUID(0xfff0))
+#for ch in testService.getCharacteristics():
+#    print (str(ch))
+                                                       
+#ch = dev.getCharacteristics(uuid=UUID(0xfff4))[0]
+'''
+handle = ch.getHandle() + 2
+res = dev.writeCharacteristic(handle, b'\x02\x00', withResponse=True)
+print('res: ', res)
+#print(ch.read())
+#ch.write(b'\x02')
+
+while True:
+    if dev.waitForNotifications(10.0):
+        print('yay')
+        break
+'''
+descriptors = dev.getDescriptors()
+print(descriptors)
+
+
+for i in descriptors:
+    print('UUID: ', i.uuid)
+    if i.uuid == UUID(0x2902):
+        try:
+            i.write(b'\x01\x00', withResponse=True)
+            #print(res)
+            value = i.read()
+            print('value: ', value)
+        
+        except:
+            print("except")
+
+while True:
+    if dev.waitForNotifications(10.0):
+        continue
+        
+dev.disconnect()
---
+++ main.cpp
@@ -0,0 +1,224 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// #include "mbed.h"
+#include <cstdint>
+#include <events/mbed_events.h>
+#include "ThisThread.h"
+#include "ble/BLE.h"
+#include "ble/services/HeartRateService.h"
+#include "ble/services/MagnetometerService.h"
+#include "ble/pretty_printer.h"
+#include "mbed-trace/mbed_trace.h"
+#include "stm32l475e_iot01_magneto.h"
+
+using namespace std::literals::chrono_literals;
+
+const static char DEVICE_NAME[] = "Heartrate";
+
+static events::EventQueue event_queue(/* event count */ 16 * EVENTS_EVENT_SIZE);
+
+class HeartrateDemo : ble::Gap::EventHandler {
+public:
+    HeartrateDemo(BLE &ble, events::EventQueue &event_queue) :
+        _ble(ble),
+        _event_queue(event_queue),
+        _heartrate_uuid(GattService::UUID_HEART_RATE_SERVICE),
+        _heartrate_value(100),
+        _magneto_uuid(GattService::UUID_ENVIRONMENTAL_SERVICE),
+        _magneto_x_value(0),
+        _magneto_y_value(0),
+        _magneto_z_value(0),
+        _magneto_service(ble),
+        _heartrate_service(ble, _heartrate_value, HeartRateService::LOCATION_FINGER),
+        _adv_data_builder(_adv_buffer)
+    {
+        BSP_MAGNETO_Init();
+    }
+
+    void start()
+    {
+        _ble.init(this, &HeartrateDemo::on_init_complete);
+
+        _event_queue.dispatch_forever();
+    }
+
+private:
+    /** Callback triggered when the ble initialization process has finished */
+    void on_init_complete(BLE::InitializationCompleteCallbackContext *params)
+    {
+        if (params->error != BLE_ERROR_NONE) {
+            printf("Ble initialization failed.");
+            return;
+        }
+
+        print_mac_address();
+
+        /* this allows us to receive events like onConnectionComplete() */
+        _ble.gap().setEventHandler(this);
+
+        /* heart rate value updated every second */
+        _event_queue.call_every(
+            1000ms,
+            [this] {
+                update_heartRate_value();
+            }
+        );
+
+        _event_queue.call_every(
+            1000ms,
+            [this] {
+                update_sensor_value();
+            }
+        );
+
+        start_advertising();
+    }
+
+    void start_advertising()
+    {
+        /* Create advertising parameters and payload */
+
+        ble::AdvertisingParameters adv_parameters(
+            ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
+            ble::adv_interval_t(ble::millisecond_t(100))
+        );
+
+        _adv_data_builder.setFlags();
+        _adv_data_builder.setAppearance(ble::adv_data_appearance_t::GENERIC_HEART_RATE_SENSOR);
+        _adv_data_builder.setLocalServiceList({&_heartrate_uuid, 1});
+        _adv_data_builder.setName(DEVICE_NAME);
+
+        /* Setup advertising */
+
+        ble_error_t error = _ble.gap().setAdvertisingParameters(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            adv_parameters
+        );
+
+        if (error) {
+            printf("_ble.gap().setAdvertisingParameters() failed\r\n");
+            return;
+        }
+
+        error = _ble.gap().setAdvertisingPayload(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            _adv_data_builder.getAdvertisingData()
+        );
+
+        if (error) {
+            printf("_ble.gap().setAdvertisingPayload() failed\r\n");
+            return;
+        }
+
+        /* Start advertising */
+
+        error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            printf("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+
+        printf("Heart rate sensor advertising, please connect\r\n");
+    }
+
+    void update_heartRate_value()
+    {
+        /* you can read in the real value but here we just simulate a value */
+        _heartrate_value++;
+
+        /*  60 <= bpm value < 110 */
+        if (_heartrate_value == 110) {
+            _heartrate_value = 60;
+        }
+
+        _heartrate_service.updateHeartRate(_heartrate_value);
+
+    }
+    void update_sensor_value()
+    {
+        int16_t pDataXYZ[3];
+        BSP_MAGNETO_GetXYZ(pDataXYZ);
+        _magneto_x_value = pDataXYZ[0];
+        _magneto_y_value = pDataXYZ[1];
+        _magneto_z_value = pDataXYZ[2];
+        
+        printf("%d, %d, %d\n", _magneto_x_value, _magneto_y_value, _magneto_z_value);
+        _magneto_service.updateHeartRate(_magneto_x_value, 0);
+        ThisThread::sleep_for(0.33);
+        _magneto_service.updateHeartRate(_magneto_y_value, 1);
+        ThisThread::sleep_for(0.33);
+        _magneto_service.updateHeartRate(_magneto_z_value, 2);
+        ThisThread::sleep_for(0.34);
+    }
+
+    /* these implement ble::Gap::EventHandler */
+private:
+    /* when we connect we stop advertising, restart advertising so others can connect */
+    virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &event)
+    {
+        if (event.getStatus() == ble_error_t::BLE_ERROR_NONE) {
+            printf("Client connected, you may now subscribe to updates\r\n");
+        }
+    }
+
+    /* when we connect we stop advertising, restart advertising so others can connect */
+    virtual void onDisconnectionComplete(const ble::DisconnectionCompleteEvent &event)
+    {
+        printf("Client disconnected, restarting advertising\r\n");
+
+        ble_error_t error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            printf("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+    }
+
+private:
+    BLE &_ble;
+    events::EventQueue &_event_queue;
+
+    UUID _heartrate_uuid;
+    uint16_t _heartrate_value;
+    HeartRateService _heartrate_service;
+
+    UUID _magneto_uuid;
+    int16_t _magneto_x_value, _magneto_y_value, _magneto_z_value;
+    MagnetoService _magneto_service;
+
+    uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
+    ble::AdvertisingDataBuilder _adv_data_builder;
+};
+
+/* Schedule processing of events from the BLE middleware in the event queue. */
+void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context)
+{
+    event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
+}
+
+int main()
+{
+    //printf("yay");
+    mbed_trace_init();
+    // printf("hello");
+    BLE &ble = BLE::Instance();
+    ble.onEventsToProcess(schedule_ble_events);
+    // printf("hi");
+    HeartrateDemo demo(ble, event_queue);
+    demo.start();
+    return 0;
+}
